db8a0105fe850ab18c741b42442a2a4dffda1770
Adam <adam.noszczynski@gmail.com>
2020-02-15 21:46:17 +0100

    manage input styles and stories

--- src/components/atoms/Input/Input.stories.js
+++ src/components/atoms/Input/Input.stories.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Input from './Input';
 
-storiesOf('Input', module).add('default', () => <Input placeholder="login" />);
+storiesOf('Input', module)
+  .add('default', () => <Input placeholder="login" />)
+  .add('search', () => <Input placeholder="search" search />);
